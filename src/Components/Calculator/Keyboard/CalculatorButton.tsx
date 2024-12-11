3396e5fb252f432df0cd0ac082ee4a74107ebebf
ActuallySilver <52727508+ActuallySilver@users.noreply.github.com>
2024-12-11 15:59:45 +0000

    add_calculator_button_functionality
    Add the functionality to make calculator button add to the display, add number 0-9

--- src/Components/Calculator/Keyboard/CalculatorButton.tsx
+++ src/Components/Calculator/Keyboard/CalculatorButton.tsx
@@ -1,5 +1,22 @@
-function CalculatorButton({}) {
-  return <div>CalculatorButton</div>;
+import { useContext } from "react";
+import "./CalculatorButton.css";
+import { CalculatorContext } from "../../../Context/CalculatorContext";
+type CalculatorButtonProps = { value: string; onClick?: () => {} };
+
+function CalculatorButton({ value, onClick }: CalculatorButtonProps) {
+  const calculatorContext = useContext(CalculatorContext);
+  if (!calculatorContext) throw new Error("calculator Context is unavailable");
+  const { setDisplayValue } = calculatorContext;
+
+  const defaultOnClick = () => {
+    setDisplayValue((currentDisplayValue) => currentDisplayValue + value);
+  };
+
+  return (
+    <button className="calculatorButton" onClick={onClick || defaultOnClick}>
+      {value}
+    </button>
+  );
 }
 
 export default CalculatorButton;
--- src/Components/Calculator/Keyboard/index.tsx
+++ src/Components/Calculator/Keyboard/index.tsx
@@ -1,7 +1,20 @@
 import CalculatorButton from "./CalculatorButton";
 
 function Keyboard() {
-  return <CalculatorButton />;
+  return (
+    <>
+      <CalculatorButton value="1" />
+      <CalculatorButton value="2" />
+      <CalculatorButton value="3" />
+      <CalculatorButton value="4" />
+      <CalculatorButton value="5" />
+      <CalculatorButton value="6" />
+      <CalculatorButton value="7" />
+      <CalculatorButton value="8" />
+      <CalculatorButton value="9" />
+      <CalculatorButton value="0" />
+    </>
+  );
 }
 
 export default Keyboard;
