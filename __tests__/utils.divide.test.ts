b1287e30a84d7620dc6f7f1a59a28631a9d22759
ActuallySilver <jason@educateit.net>
2024-12-13 14:10:52 +0000

    add_multiply_and_divide_utils
    Add multiply and divide utils with unit tests

---
+++ __tests__/utils.divide.test.ts
@@ -0,0 +1,45 @@
+const { divide } = require("../src/utils/divide");
+describe("divide", () => {
+  test("divide 2 positive integers", () => {
+    const num1 = 4;
+    const num2 = 2;
+    const result = divide(num1, num2);
+    expect(result).toBe(2);
+  });
+  test("divide 2 negative integers", () => {
+    const num1 = -4;
+    const num2 = -2;
+    const result = divide(num1, num2);
+    expect(result).toBe(2);
+  });
+  test("divide a positive and a negative integer", () => {
+    const num1 = -4;
+    const num2 = 2;
+    const result = divide(num1, num2);
+    expect(result).toBe(-2);
+  });
+  test("divide 2 positive decimals resulting in whole number", () => {
+    const num1 = 2.8;
+    const num2 = 1.4;
+    const result = divide(num1, num2);
+    expect(result).toBe(2);
+  });
+  test("divide 2 positive decimals resulting in decimal number", () => {
+    const num1 = 1.8;
+    const num2 = 1.2;
+    const result = divide(num1, num2);
+    expect(result).toBe(1.5);
+  });
+  test("divide 0 with an integer", () => {
+    const num1 = 0;
+    const num2 = 2;
+    const result = divide(num1, num2);
+    expect(result).toBe(0);
+  });
+  test("divide by 0", () => {
+    const num1 = 2;
+    const num2 = 0;
+    const result = divide(num1, num2);
+    expect(result).toBe(NaN);
+  });
+});
---
+++ __tests__/utils.multiply.test.ts
@@ -0,0 +1,39 @@
+const { multiply } = require("../src/utils/multiply");
+describe("multiply", () => {
+  test("multiply 2 positive integers", () => {
+    const num1 = 1;
+    const num2 = 2;
+    const result = multiply(num1, num2);
+    expect(result).toBe(2);
+  });
+  test("multiply 2 negative integers", () => {
+    const num1 = -1;
+    const num2 = -2;
+    const result = multiply(num1, num2);
+    expect(result).toBe(2);
+  });
+  test("multiply a positive and a negative integer", () => {
+    const num1 = -1;
+    const num2 = 2;
+    const result = multiply(num1, num2);
+    expect(result).toBe(-2);
+  });
+  test("multiply 2 positive decimals resulting in whole number", () => {
+    const num1 = 1.2;
+    const num2 = 2.5;
+    const result = multiply(num1, num2);
+    expect(result).toBe(3);
+  });
+  test("multiply 2 positive decimals resulting in decimal number", () => {
+    const num1 = 1.4;
+    const num2 = 2.5;
+    const result = multiply(num1, num2);
+    expect(result).toBe(3.5);
+  });
+  test("multiply 0 with an integer", () => {
+    const num1 = 0;
+    const num2 = 2;
+    const result = multiply(num1, num2);
+    expect(result).toBe(0);
+  });
+});
