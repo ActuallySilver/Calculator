ccfcb884fff96dbae931415519bef066eea398e1
ActuallySilver <jason@educateit.net>
2024-12-14 19:20:00 +0000

    refactor_calculate
    refactor calculate to make code easier to read and separate out functionality

--- src/utils/calculate.ts
+++ src/utils/calculate.ts
@@ -3,70 +3,82 @@ import { divide } from "./divide";
 import { multiply } from "./multiply";
 import { subtract } from "./subtract";
 
+const scientificNotationRegex = /(^-)?\d+(\.\d+)?(e[+-]?\d+)?/;
+
 export const calculate = (equation: string): string => {
-  const scientificNotationRegex = /(^-)?\d+(\.\d+)?(e[+-]?\d+)?/;
-  const invalidCharacterRegex = /[\d×÷*/\+\-\(\)\.e]+/;
-  const innerMostBracketsRegex = /\([\d+-×÷\.]+\)/;
+  equation = formatEquation(equation);
 
-  //If answer is xe+y (×10^y) return it
-  if (new RegExp(`^${scientificNotationRegex.source}$`).test(equation)) {
-    return equation;
-  }
-
-  //remove invalid characters. If all characters invalid then set equation as "0"
-  [equation] = equation.match(new RegExp(invalidCharacterRegex)) || "0";
-
-  equation = equation.replace(/--/g, '+');
-
-  let equationSection = null;
-
-  //Checking for inner most brackets and resolving as "new equation"
-  equationSection = equation.match(new RegExp(innerMostBracketsRegex));
-  if (equationSection) {
-    const result = calculate(equationSection[0].replace(/[\(\)]/g, ""));
-    const newEquation = equation.replace(equationSection[0], result.toString());
-    return calculate(newEquation);
-  }
-
-  //Checking for multiply and divide
-  equationSection =
-    equationSection || equation.match(new RegExp(`${scientificNotationRegex.source}[×÷*/]${scientificNotationRegex.source}`));
-  //checking for add or subtract
-  equationSection =
-    equationSection || equation.match(new RegExp(`(${scientificNotationRegex.source})[\+\-](${scientificNotationRegex.source})`));
-
+  const equationSection = getNextEquationSection(equation);
+  
   //Equation has nothing left to resolve
   if (!equationSection) {
     return equation;
   }
 
-  const [symbol] = equationSection[0].replace(new RegExp(scientificNotationRegex.source, "g"), "").match(/[\+\-×*/÷]/)!;
+  const { symbol, num1, num2 } = getEquationParts(equationSection);
 
-  const numbersIterator = equationSection[0].matchAll(new RegExp(scientificNotationRegex.source, "g"));
+  let result = 0;
+  switch (symbol) {
+    case "+":
+      result = add(num1, num2);
+      break;
+    case "-":
+      result = subtract(num1, num2);
+      break;
+    case "×":
+    case "*":
+      result = multiply(num1, num2);
+      break;
+    case "÷":
+    case "/":
+      result = divide(num1, num2);
+      break;
+  }
+
+  //Replace section of equation that was calculated with the result
+  const newEquation = equation.replace(equationSection, `${result}`);
+  return calculate(newEquation);
+};
+
+const formatEquation = (equation: string): string => {
+  //remove invalid characters. If all characters invalid then set equation as "0"
+  const invalidCharacterRegex = /[\d×÷*/\+\-\(\)\.e]+/;
+  [equation] = equation.match(new RegExp(invalidCharacterRegex)) || "0";
+
+  equation = equation.replace(/--/g, "+");
+  return equation;
+};
+
+//Returns all parts of the equation
+const getEquationParts = (equation: string) => {
+  const [symbol] = equation.replace(new RegExp(scientificNotationRegex.source, "g"), "").match(/[\+\-×*/÷]/)!;
+
+  const numbersIterator = equation.matchAll(new RegExp(scientificNotationRegex.source, "g"));
   const numbers: number[] = [];
   for (let number of numbersIterator) {
     numbers.push(Number.parseFloat(number[0]));
   }
 
-  let result = 0;
-  switch (symbol) {
-    case "+":
-      result = add(numbers[0], numbers[1]);
-      break;
-    case "-":
-      result = subtract(numbers[0], numbers[1]);
-      break;
-    case "×":
-    case "*":
-      result = multiply(numbers[0], numbers[1]);
-      break;
-    case "÷":
-    case "/":
-      result = divide(numbers[0], numbers[1]);
-      break;
-  }
+  return { symbol, num1: numbers[0], num2: numbers[1] };
+};
 
-  //Replace section of equation that was calculated with the result
-  const newEquation = equation.replace(equationSection[0], `${result}`);
-  return calculate(newEquation);
+//Gets the next part of the equation to resolve
+const getNextEquationSection = (equation: string) => {
+  const innerMostBracketsRegex = /\([\d+-×÷\.]+\)/;
+
+  let equationSection = null;
+
+  //Checking for inner most brackets and resolving as "new equation"
+  equationSection = equation.match(new RegExp(innerMostBracketsRegex));
+
+  //Checking for multiply and divide
+  equationSection =
+    equationSection || equation.match(new RegExp(`${scientificNotationRegex.source}[×÷*/]${scientificNotationRegex.source}`));
+
+  //checking for add or subtract
+  equationSection =
+    equationSection || equation.match(new RegExp(`(${scientificNotationRegex.source})[\+\-](${scientificNotationRegex.source})`));
+
+  if (!equationSection) return null;
+  return equationSection[0];
 };
