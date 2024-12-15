c9760dc7c5020f512c99146644c647361143d2c7
ActuallySilver <jason@educateit.net>
2024-12-15 12:30:07 +0000

    Add-GH-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Calculator",
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
